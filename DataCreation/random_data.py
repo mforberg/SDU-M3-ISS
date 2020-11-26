2c7277944457a5a88e916060a72a4bec929d26a8
JorgeBaezGarrido <joole16@student.sdu.dk>
2020-11-26 13:48:17 +0100

    More added

--- DataCreation/random_data.py
+++ DataCreation/random_data.py
@@ -5,23 +5,33 @@ import string
 number_of_items = 2000
 
 uuid = 1
+uuid_list = list()
 color_list = ["black", "blue", "brown", "green", "red", "pink", "purple", "yellow", "white"]
 brand_list = ["Addidas", "Nike", "Puma", "Reebok", "Jack&Jones", "Boss", "Levi's", "Lacoste", "Ralph Lauren", "Lee", "Zara"]
 men_category = ["T-shirt", "jeans", "pants", "underpants", "shirt", "suits", "shoes", "hoodies"]
 women_category = ["dress", "shirts", "jackets", "hoodies", "pants", "skirts", "shoes"]
 children_catgory = ["size 50-74", "size 68-104", "size 92-140", "size 134-170"]
 all_categories = [men_category, women_category, children_catgory]
-price = 1
+price = 2
 style = ["casual", "sporty"]
+rand = 1429
+
 
 def lcg(x):
     a = 18
     c = 11
-    m = 17
-    rand = (a * x + c) % m
-    return rand
+    m = 10007
+    x = (a * x + c) % m
+    return x
 
 
 for x in range(number_of_items):
+
+    while len(uuid_list) < x:
+        rand = lcg(1429)
+        if rand not in uuid_list:
+            uuid_list.append
+    
     uuid = random.randint(1, 9999)
+    
 

efad7a6277ac8b13029ecec0d379dc108262715a
SmiteyJonawar <jonasr16@student.sdu.dk>
2020-12-07 20:30:44 +0100

    WIP with register

--- LewdBokks/forms.py
+++ LewdBokks/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField
+from wtforms import PasswordField, StringField, SubmitField, validators, BooleanField
 from wtforms.validators import DataRequired, Length
 
 
@@ -16,3 +16,26 @@ class LoginForm(FlaskForm):
     )
 
     submit = SubmitField('Submit')
+
+
+class RegisterPersonForm(FlaskForm):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+
+
+class RegisterCompany(FlaskForm):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    companyname = StringField('Company name', [validators.Length(min=4, max=25)])
+    websiteurl = StringField('Website URL', [validators.Length(min=6, max=100)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])

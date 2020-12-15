c2489680c1d12eb2579adda0032e21280c7fd94a
Zackzter <jorge.olesen@hotmail.com>
2020-12-15 15:52:05 +0100

    change for random, and todo gone

--- LewdBokks/backend/lootboxes.py
+++ LewdBokks/backend/lootboxes.py
@@ -25,25 +25,24 @@ class LootBox:
         preferences = self.__get_preferences(uuid, dbc)
         collected = self.__collect(preferences, dbc)
         probability_assigned_items = self.__set_probabilities(collected, dbc)
-        #collected_items = self.__collect(probability_assigned_items, dbc)
         drawn_items = self.__draw_items(probability_assigned_items, lootbox_type)
         dbc.add_lootbox(uuid, drawn_items[1], drawn_items[0], drawn_items[2])
         return drawn_items
 
     def __get_preferences(self, uuid, dbc):
-        """TODO: Needs to collect the users preferences from a database, based on their UUID"""
+        """Collects the users preferences from a database, based on their UUID"""
         result = dbc.get_preferences(uuid)
         return result
 
     def __collect(self, preferences, dbc):
-        """TODO: Needs to collect all (?) articles of clothing based on the preferences of the user prefs"""
+        """Collects all articles of clothing based on the preferences of the user prefs"""
         result = []
         for preference in preferences:
             result.append(dbc.get_preferences_items(preference))
         return result
 
     def __set_probabilities(self, collected_items, dbc):
-        """TODO: Once items has been collected, they need to have assigned random probabilities between their min &
+        """Once items has been collected, they need to have assigned random probabilities between their min &
         max """
         result = []
         for item in collected_items:
@@ -52,14 +51,12 @@ class LootBox:
         return result
 
     def __draw_items(self, probabilitiy_assigned_items, lootbox_type):
-        """TODO: Should draw items based on probabilities"""
-
-        
+        """Draw items based on probabilities"""        
         item = random.choice(probabilitiy_assigned_items)
         discount = 0
         mini = item[1][0][0]
         maxi = item [1][0][1]
-        val = random.randint(0, 100)
+        val = random.randint(1, 100)
         loot = ""
         if lootbox_type == "Common":
             if val <= 80:
